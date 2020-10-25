9dbb084b3b62d62e7cfa64a4b06259a81802861e
DanielJDufour <daniel.j.dufour@gmail.com>
2020-10-25 13:01:27 -0400

    updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
-const path = require('path');
-const { readFileSync } = require('fs');
+const path = require("path");
+const { readFileSync } = require("fs");
 
 module.exports = {
-  entry: './georaster-layer-for-leaflet.js',
+  watch: process.env.WEBPACK_WATCH === "true",
+  entry: "./georaster-layer-for-leaflet.js",
   output: {
-    filename: 'georaster-layer-for-leaflet.min.js',
-    path: path.resolve(__dirname),
+    filename: "georaster-layer-for-leaflet.min.js",
+    path: path.resolve(__dirname)
   },
+  devtool: "source-map",
   module: {
     rules: [
-        {
-            test: /\.m?js$/,
-            use: {
-                loader: 'babel-loader',
-                options: JSON.parse(readFileSync(".babelrc.json", 'utf-8'))
-            }
+      {
+        test: /\.m?js$/,
+        use: {
+          loader: "babel-loader",
+          options: JSON.parse(readFileSync(".babelrc.json", "utf-8"))
+        }
       }
-    ]      
+    ]
   }
 };
